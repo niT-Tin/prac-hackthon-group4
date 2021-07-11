7921cfe27dc979da16e461204602c6fc0caabc76
liuzehao <1437070389@qq.com>
2021-07-11 17:43:41 +0800

    the models of the databases and data of the return type

---
+++ models/data.go
@@ -0,0 +1,27 @@
+package models
+
+import "gorm.io/gorm"
+
+type G4Data struct {
+	ErrorCode int    `json:"error_code"`
+	Message   string `json:"message"`
+	DataU     []User `json:"datau,omitempty"`
+	DataT     []Text `json:"datat,omitempty"`
+}
+
+type User struct {
+	gorm.Model
+	ID         int    `gorm:"primary_key" json:"id"`
+	UserName   string `json:"user_name"`
+	password   string
+	AvatarUrl  string `json:"avatar_url"`
+	ActiveDays int    `json:"active_days"`
+	TextCount  int    `json:"text_count"`
+	TextId     int    `gorm:"unique" json:"text_id"`
+}
+
+type Text struct {
+	gorm.Model
+	TextId   int    `json:"text_id"`
+	TextData string `json:"text"`
+}
